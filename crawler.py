2132f29c3af4106cc294fcaa0c3b74e33aae184d
unknown <gvsamardzh@uni-sofia.bg>
2021-02-19 00:52:34 +0200

    Change some names

---
+++ crawler.py
@@ -0,0 +1,34 @@
+from movie_title_scraper import MovieTitleScraper
+from rotten_tomatoes_scraper import RottenTomatoesMovieScrapper
+import json
+import os  
+
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+
+def crawl(years):
+    movie_title_scraper = MovieTitleScraper()
+    movie_info_scraper = RottenTomatoesMovieScrapper()
+
+    for year in years:
+        try:
+            os.mkdir(f'{year}')
+        except:
+            print('Could not create folder')
+
+    for year in years:
+        names = movie_title_scraper.scrape_movie_name_for_year(year)
+        for name in names:
+            try:
+                info = movie_info_scraper.get_movie_info(name)
+                f = open(os.path.join(fileDir, f'{year}\\{name}.json'), "w+")
+                f.write(json.dumps(info))
+                f.close()
+            except:
+                print(f'no such film: {name}')
+
+
+
+
+if __name__ == "__main__":
+    years = [1989, 1990,1991,1992,1993,1994,1995,1996,1997, 1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
+    crawl(years)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from crawer import craw
+from crawler import crawl
 from preprocess_scraped_data import get_review_sentiment_dict, clear_scraped_data
 from sentiment_analysis import vectorizers_and_predictors, train_models, preprocess, load_model_by_vectorizer_and_predictor, number_to_word_category
 import pandas as pd 
 
 if __name__ == "__main__":
     years = [1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
-    craw(years)
+    crawl(years)
     clear_scraped_data(years)
     crawed_df = get_review_sentiment_dict(years)
     reviews_df = pd.read_csv('train.tsv', delimiter='\t')
